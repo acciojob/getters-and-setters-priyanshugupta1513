2f96ec3103cf5bfcbc9896b000426106fb360a7c
priyanshugupta1513 <priyanshu35828@gmail.com>
2025-06-08 19:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 //complete this code
 class Person {
-	constructor(name:string, age:number){
+	constructor(name, age){
 		this.name=name;
 		this.age=age;
 	}
 	getName(){
 		return this.name;
 	}
-	setAge(age:number){
+	setAge(age){
 		this.age=age;
 	}
 }
 
 class Student extends Person {
-
-	
-
 	study(){
 		console.log(this.name+" is studying");
 	}
 }
 
 class Teacher extends Person {
-
 	teach(){
 		console.log(this.name+" is teaching");
 	}
